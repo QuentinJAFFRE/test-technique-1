f75f865305c8e8b7206c8f0b9388c2958c45cd5e
Quentin Jaffré <quentin.jaffre@epita.fr>
2023-02-28 19:05:09 +0100

    fix: email is not mandatory

--- app/src/scenes/account.js
+++ app/src/scenes/account.js
@@ -36,52 +36,50 @@ export default () => {
           }}>
           {({ values, errors, isSubmitting, handleChange, handleSubmit }) => (
             <form onSubmit={handleSubmit}>
-              <React.Fragment>
-                {/* First Row */}
-                <div className="flex justify-between flex-wrap mt-3">
-                  <div className="w-full md:w-[48.5%]">
-                    <div>Name</div>
-                    <Field
-                      className="projectsInput"
-                      validate={(v) => validator.isEmpty(v) && "This field is Required"}
-                      id="name"
-                      name="name"
-                      value={values.name}
-                      onChange={handleChange}
-                    />
-                    {/* Error */}
-                    <p className="text-[12px] text-[#FD3131] my-1">{errors.name}</p>
-                  </div>
-                  <div className="w-full md:w-[48.5%]">
-                    <div>Email</div>
-                    <Field
-                      className="projectsInput"
-                      validate={(v) => !validator.isEmail(v) && "This must be a valid email"}
-                      name="email"
-                      value={values.email}
-                      onChange={handleChange}
-                    />
-                    {/* Error */}
-                    <p className="text-[12px] text-[#FD3131] my-1">{errors.email}</p>
-                  </div>
+              {/* First Row */}
+              <div className="flex justify-between flex-wrap mt-3">
+                <div className="w-full md:w-[48.5%]">
+                  <div>Name</div>
+                  <Field
+                    className="projectsInput"
+                    validate={(v) => validator.isEmpty(v) && "This field is Required"}
+                    id="name"
+                    name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                  />
+                  {/* Error */}
+                  <p className="text-[12px] text-[#FD3131] my-1">{errors.name}</p>
                 </div>
-                {/* second Row */}
-                <div className="flex justify-between flex-wrap mt-3">
-                  <div className="w-full md:w-[48.5%]">
-                    <div>Address</div>
-                    <textarea className="projectsInput h-auto py-2" name="address" value={values.address} onChange={handleChange} />
-                  </div>
+                <div className="w-full md:w-[48.5%]">
+                  <div>Email</div>
+                  <Field
+                    className="projectsInput"
+                    validate={(v) => !validator.isEmail(v) && !validator.isEmpty(v) && "This must be a valid email"}
+                    name="email"
+                    value={values.email}
+                    onChange={handleChange}
+                  />
+                  {/* Error */}
+                  <p className="text-[12px] text-[#FD3131] my-1">{errors.email}</p>
                 </div>
-                <hr className="my-4" />
-                <div className="flex justify-end">
-                  <LoadingButton
-                    className="ml-[10px] bg-[#17a2b8] hover:bg-[#138496] text-[1rem] text-[#fff] py-[0.375rem] px-[0.75rem] rounded-[0.25rem]"
-                    loading={isSubmitting}
-                    type="submit">
-                    Update
-                  </LoadingButton>
+              </div>
+              {/* second Row */}
+              <div className="flex justify-between flex-wrap mt-3">
+                <div className="w-full md:w-[48.5%]">
+                  <div>Address</div>
+                  <textarea className="projectsInput h-auto py-2" name="address" value={values.address} onChange={handleChange} />
                 </div>
-              </React.Fragment>
+              </div>
+              <hr className="my-4" />
+              <div className="flex justify-end">
+                <LoadingButton
+                  className="ml-[10px] bg-[#17a2b8] hover:bg-[#138496] text-[1rem] text-[#fff] py-[0.375rem] px-[0.75rem] rounded-[0.25rem]"
+                  loading={isSubmitting}
+                  type="submit">
+                  Update
+                </LoadingButton>
+              </div>
             </form>
           )}
         </Formik>
